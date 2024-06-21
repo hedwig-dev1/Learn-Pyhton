ffd5115fe6dc35fd41c420bd9102380127e6567d
rijalulakhri-dev <shfdell@gmail.com>
2024-06-21 14:51:48 +0700

    first commit

---
+++ operator.py
@@ -0,0 +1,12 @@
+# Operator
+# f-string di gunakan untuk mnyisipkan nilai variabel
+a = int(input('Masukkan nilai A: '))
+b = int(input('Masukkan nilai B: '))
+c = int(input('Masukkan nilai C: '))
+
+hasil_jumlah = a + b + c
+
+# Format string untuk output yang diinginkan
+output = f"Hasil penambahan {a} + {b} + {c} : {hasil_jumlah}"
+
+print(output)
