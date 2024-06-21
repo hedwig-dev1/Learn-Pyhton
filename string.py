97e72936339bd7da22a9fb63b7726c7e7ad493b4
rijalulakhri-dev <shfdell@gmail.com>
2024-06-21 15:09:03 +0700

    commit

---
+++ string.py
@@ -0,0 +1,10 @@
+# String sederhana
+nama = "John"
+print(f"Halo, {nama}!")
+
+# String multi-line dengan tiga tanda kutip ganda
+deskripsi = """
+Kursus ini akan mengajarkan Anda cara memprogram dengan bahasa Python.
+Anda akan belajar tentang sintaks dasar, struktur data, algoritma, dan banyak lagi.
+"""
+print(deskripsi)
---
+++ variebel.py
@@ -0,0 +1,21 @@
+# Penulisan Variabel
+# BENAR
+
+nama = 'Rijalul Akhri'
+
+# SALAH
+# nama lengkap = 'Rijalul Akhri'
+
+
+# PENULISAN CASE
+
+# Snake Case 
+# contoh : nama_lengkap
+
+# CamelCase
+# contoh : namaLengkap
+
+# ALL CAPS : PI, MAX_VALUE
+
+
+# NOTE : ALL CAPS DI GUNAKAN JIKA NILAI DARI SEBUAH VARIABEL TIDAK BERUBAH SEPANJANG PROGRAM
